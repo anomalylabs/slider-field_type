a8d56e53cdecf26d06f1662aec851bb5f847db39
Ryan Thompson <service@aiwebsystems.com>
2014-08-22 20:22:35 -0500

    Initial commit.

---
+++ src/SliderFieldType.php
@@ -0,0 +1,30 @@
+<?php namespace Addon\FieldType\Slider;
+
+use Streams\Core\Addon\FieldTypeAbstract;
+
+class SliderFieldType extends FieldTypeAbstract
+{
+    /**
+     * The database column type this field type uses.
+     *
+     * @var string
+     */
+    public $columnType = 'slider';
+
+    /**
+     * Field type version
+     *
+     * @var string
+     */
+    public $version = '1.1.0';
+
+    /**
+     * Field type author information.
+     *
+     * @var array
+     */
+    public $author = array(
+        'name' => 'AI Web Systems, Inc.',
+        'url'  => 'http://aiwebsystems.com/',
+    );
+}
