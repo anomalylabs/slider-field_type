a627005b2bc9d5539213255a159d4c8348f891e7
Ryan Thompson <service@aiwebsystems.com>
2015-01-05 01:56:39 -0600

    View changes

--- src/SliderFieldType.php
+++ src/SliderFieldType.php
@@ -19,25 +19,4 @@ class SliderFieldType extends FieldType
      * @var string
      */
     protected $inputView = 'anomaly.field_type.slider::input';
-
-    /**
-     * Get view data for the input.
-     *
-     * @return array
-     */
-    public function getInputData()
-    {
-        $data = parent::getInputData();
-
-        $data['min']  = $this->pullConfig('min', 0);
-        $data['max']  = $this->pullConfig('max', 10);
-        $data['step'] = $this->pullConfig('step', 1);
-
-        $data['minLabel'] = trans($this->pullConfig('min_label', 'misc.min'));
-        $data['maxLabel'] = trans($this->pullConfig('max_label', 'misc.max'));
-
-        $data['sliderValue'] = str_contains($data['value'], ',') ? "[{$data['value']}]" : $data['value'];
-
-        return $data;
-    }
 }
